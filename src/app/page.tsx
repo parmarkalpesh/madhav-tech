0ae9b373463e2e5fbf1973703d940579c8d49f61
Parmar Kalpesh <parmarkalpesh1586@gmail.com>
2025-10-25 18:02:34 +0000

    add SEO for reach internatiomal client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,38 @@ import Services from '@/components/site/services';
 import Projects from '@/components/site/projects';
 import Contact from '@/components/site/contact';
 import Footer from '@/components/site/footer';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'MadhavTech | Global IT Solutions & Web Development',
+  description: "MadhavTech provides top-tier global IT solutions, including web and app development, and UI/UX design. We partner with international clients to deliver innovative and scalable technology services.",
+  keywords: ['IT solutions', 'web development', 'app development', 'UI/UX design', 'international clients', 'global software company', 'technology services', 'offshore development', 'custom software development', 'IT consulting', 'nearshore outsourcing', 'dedicated development team'],
+};
+
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "MadhavTech",
+  "url": "https://www.madhav.tech", // Replace with your actual domain
+  "logo": "https://www.madhav.tech/logo.png", // Replace with a link to your logo
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+91-7016223029",
+    "contactType": "Customer Service"
+  },
+  "sameAs": [
+    "https://www.linkedin.com/company/madhav-tech/",
+    "https://www.instagram.com/madhav.tech_/"
+  ]
+};
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
       <Header />
       <main className="flex-1">
         <Hero />
